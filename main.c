9c5928d332150855df311eefbfd7ab9316190bdc
stajw <stajw@naver.com>
2016-06-24 01:43:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -23,116 +23,116 @@ int main(){
 		int tweetnum = 0;
 		int friendnum = 0;
 		int userid[10000] = { 0 };
-		int useridcnt[10000] = { -1 };
-		int tweetcnt[10000] = { -1 };
+		int useridcnt[10000] = { 0 };
+		int tweetcnt[10000] = { 0 };
 		int useridnum = 0;
 		int maxfriend, minfriend, maxtweet, mintweet;
 
-			switch (menu){
-			case 0: {
-				printf("\n");
-				char arr[1000];
-				char *blk;
+		switch (menu){
+		case 0: {
+			printf("\n");
+			char arr[1000];
+			char *blk;
 
-				FILE *fp = fopen("C:\\ds\\user.txt", "r");
+			FILE *fp = fopen("C:\\ds\\user.txt", "r");
 
-				while (fgets(arr, sizeof(arr), fp) != NULL){
-					if (!(arr[0] == '\n' || arr[0] == ' ')){
-						int ID = atoi(arr);
-						userid[useridnum++] = ID;
-						fgets(arr, sizeof(arr), fp);
-						fgets(arr, sizeof(arr), fp);
+			while (fgets(arr, sizeof(arr), fp) != NULL){
+				if (!(arr[0] == '\n' || arr[0] == ' ')){
+					int ID = atoi(arr);
+					userid[useridnum++] = ID;
+					fgets(arr, sizeof(arr), fp);
+					fgets(arr, sizeof(arr), fp);
 
-						if ((blk = strchr(arr, '\n')) != NULL) *blk = '\0';
-						usernum++;
-					}
+					if ((blk = strchr(arr, '\n')) != NULL) *blk = '\0';
+					usernum++;
 				}
-				printf("total users : %d\n", usernum);
-				fclose(fp);
-
-				fp = fopen("C:\\ds\\friend.txt", "r");
-
-				while (fgets(arr, sizeof(arr), fp) != NULL){
-					if (!(arr[0] == ' ' || arr[0] == '\n')){
-						int firstID = atoi(arr);
-						for (int i = 0; i < 10000; i++){
-							if (firstID == userid[i]){
-								useridcnt[i]++;
-								break;
-							}
-						}
-						fgets(arr, sizeof(arr), fp);
-						int secondID = atoi(arr);
-						for (int i = 0; i < 10000; i++){
-							if (secondID == userid[i]){
-								useridcnt[i]++;
-								break;
-							}
-						}
-						friendnum++;
-					}
-				}
-				printf("Total friendship records : %d\n", friendnum);
-				fclose(fp);
-
-				fp = fopen("C:\\ds\\word.txt", "r");
-
-				while (fgets(arr, sizeof(arr), fp) != NULL){
-					if (!(arr[0] == ' ' || arr[0] == '\n')){
-						int tweetID = atoi(arr);
-						for (int i = 0; i < 10000; i++){
-							if (tweetID == userid[i]){
-								tweetcnt[i]++;
-								break;
-							}
-						}
-						fgets(arr, sizeof(arr), fp);
-						fgets(arr, sizeof(arr), fp);
-
-						if ((blk = strchr(arr, '\n')) != NULL) *blk = '\0';
-						tweetnum++;
-					}
-				}
-				printf("Total tweets : %d\n", tweetnum);
-				fclose(fp);
-
-				avgfriend = friendnum / usernum;
-				avgtweet = tweetnum / usernum;
-
-				maxfriend = useridcnt[0];
-				minfriend = useridcnt[0];
-				for (int i = 1; i < 10000; i++){
-					if (useridcnt[i] > maxfriend) maxfriend = useridcnt[i];
-				}
-				for (int i = 1; i < 10000; i++){
-					if (useridcnt[i] != -1 && useridcnt[i] < minfriend) minfriend = useridcnt[i];
-				}
-
-				maxtweet = tweetcnt[0];
-				mintweet = tweetcnt[0];
-				for (int i = 1; i < 10000; i++){
-					if (tweetcnt[i] > maxtweet) maxtweet = tweetcnt[i];
-				}
-				for (int i = 1; i < 10000; i++){
-					if (tweetcnt[i] != -1 && tweetcnt[i] < mintweet) mintweet = tweetcnt[i];
-				}
-				printf("\n");
-
 			}
-					break;
-			case 1: {
-				printf("\n");
-				printf("Average number of friends: %d\n", avgfriend);
-				printf("Minimum friends : %d\n", minfriend + 1);
-				printf("Maximum friends : %d\n", maxfriend + 1);
-				printf("Average tweets per user : %d\n", avgtweet);
-				printf("Minimum tweets per user : %d\n", mintweet + 1);
-				printf("Maximum tweets per user : %d\n", maxtweet + 1);
-				printf("\n");
+			printf("total users : %d\n", usernum);
+			fclose(fp);
+
+			fp = fopen("C:\\ds\\friend.txt", "r");
+
+			while (fgets(arr, sizeof(arr), fp) != NULL){
+				if (!(arr[0] == ' ' || arr[0] == '\n')){
+					int firstID = atoi(arr);
+					for (int i = 0; i < 10000; i++){
+						if (firstID == userid[i]){
+							useridcnt[i]++;
+							break;
+						}
+					}
+					fgets(arr, sizeof(arr), fp);
+					int secondID = atoi(arr);
+					for (int i = 0; i < 10000; i++){
+						if (secondID == userid[i]){
+							useridcnt[i]++;
+							break;
+						}
+					}
+					friendnum++;
+				}
 			}
-					break;
+			printf("Total friendship records : %d\n", friendnum);
+			fclose(fp);
+
+			fp = fopen("C:\\ds\\word.txt", "r");
+
+			while (fgets(arr, sizeof(arr), fp) != NULL){
+				if (!(arr[0] == ' ' || arr[0] == '\n')){
+					int tweetID = atoi(arr);
+					for (int i = 0; i < 10000; i++){
+						if (tweetID == userid[i]){
+							tweetcnt[i]++;
+							break;
+						}
+					}
+					fgets(arr, sizeof(arr), fp);
+					fgets(arr, sizeof(arr), fp);
+
+					if ((blk = strchr(arr, '\n')) != NULL) *blk = '\0';
+					tweetnum++;
+				}
 			}
-		}while (menu != 99);
+			printf("Total tweets : %d\n", tweetnum);
+			fclose(fp);
+
+			avgfriend = (friendnum*2) / usernum;
+			avgtweet = tweetnum / usernum;
+
+			maxfriend = useridcnt[0];
+			minfriend = useridcnt[0];
+			for (int i = 1; i < usernum; i++){
+				if (useridcnt[i] > maxfriend) maxfriend = useridcnt[i];
+			}
+			for (int i = 1; i < usernum; i++){
+				if (useridcnt[i] < minfriend) minfriend = useridcnt[i];
+			}
+
+			maxtweet = tweetcnt[0];
+			mintweet = tweetcnt[0];
+			for (int i = 1; i < usernum; i++){
+				if (tweetcnt[i] > maxtweet) maxtweet = tweetcnt[i];
+			}
+			for (int i = 1; i < usernum; i++){
+				if (tweetcnt[i] < mintweet) mintweet = tweetcnt[i];
+			}
+			printf("\n");
+
+		}
+				break;
+		case 1: {
+			printf("\n");
+			printf("Average number of friends: %d\n", avgfriend);
+			printf("Minimum friends : %d\n", minfriend);
+			printf("Maximum friends : %d\n", maxfriend);
+			printf("Average tweets per user : %d\n", avgtweet);
+			printf("Minimum tweets per user : %d\n", mintweet);
+			printf("Maximum tweets per user : %d\n", maxtweet);
+			printf("\n");
+		}
+				break;
+		}
+	} while (menu != 99);
 
 	return 0;
 }
